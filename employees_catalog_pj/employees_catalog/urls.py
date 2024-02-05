9c961cfcff7ba153b351cd3052f05f3d3b6049de
Ivan Timofeev <timofeevis89@gmail.com>
2024-02-05 14:17:11 +0300

    sorting and searching implementation

--- employees_catalog_pj/employees_catalog/urls.py
+++ employees_catalog_pj/employees_catalog/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('catalog/', views.EmpCatalogListView.as_view(), name='emp_catalog'),
+    path('catalog-detail/', views.CatalogDetailListView.as_view(), name='catalog_detail'),
 ]
--- employees_catalog_pj/employees_catalog/views.py
+++ employees_catalog_pj/employees_catalog/views.py
@@ -1,4 +1,4 @@
-
+from django.db.models import Q
 from django.views.generic import ListView
 
 from .models import Employees
@@ -11,3 +11,29 @@ class EmpCatalogListView(ListView):
 
     def get_queryset(self):
         return Employees.objects.filter(hierarchy_level=0)
+
+
+class CatalogDetailListView(ListView):
+    template_name = 'employees_catalog/catalog_detail.html'
+    model = Employees
+    context_object_name = 'employees'
+    ordering = ['name']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        order_by = self.request.GET.get('order_by')
+        order_direction = self.request.GET.get('order_direction', 'asc')
+        search_query = self.request.GET.get('search_query')
+
+        if order_by:
+            queryset = queryset.order_by(f"{'' if order_direction == 'asc' else '-'}{order_by}")
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) |
+                Q(position__icontains=search_query) |
+                Q(hire_date__icontains=search_query) |
+                Q(salary__icontains=search_query)
+            )
+
+        return queryset
